99fc76bf69135d28691f1b41877024d462941ef2
rafmontenegro <rafaelcouto111@gmail.com>
2023-06-28 15:59:43 -0300

    Module_2 init

---
+++ Module_2/mycontacts/src/app/controllers/ContactController.js
@@ -0,0 +1,19 @@
+class ContactController {
+  index() {
+    // listar todos os registros
+  }
+
+  show() {
+    // obter UM registro
+  }
+
+  store() {
+    // criar novo registro
+  }
+
+  update() {
+    // editar um programa
+  }
+}
+
+module.exports = new ContactController();
