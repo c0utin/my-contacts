6849c52d973e1e5c86d24674e17b0e41452a5cf7
rafmontenegro <rafaelcouto111@gmail.com>
2023-12-29 10:41:43 -0300

    feat: header contacts list

---
+++ ui/src/components/ContacstsList/index.js
@@ -0,0 +1,12 @@
+import { Container, Header } from "./styles";
+
+export default function ContatcsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contacts</strong>
+        <a href="/">New contact</a>
+      </Header>
+    </Container>
+  );
+}
