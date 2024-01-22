7af68de7f529323c0297212bec76f2dafe196f90
rafmontenegro <rafaelcouto111@gmail.com>
2024-01-22 16:42:38 -0300

    ref: [ui]move InputSearch to Home

--- ui/src/pages/Home/index.js
+++ ui/src/pages/Home/index.js
@@ -1,3 +1,52 @@
+import {
+  Container,
+  InputSearchContainer,
+  Header,
+  ListContainer,
+  Card,
+} from "./styles";
+
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
+
 export default function Home() {
-  return "Homepage";
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Search by name..." />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 Contacts</strong>
+        <a href="/">New contact</a>
+      </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Name</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+      </ListContainer>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Rafael Montenegro</strong>
+            <small>instagram</small>
+          </div>
+          <span>rafael@dev.com</span>
+          <span>(99) 999999-999999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+    </Container>
+  );
 }
