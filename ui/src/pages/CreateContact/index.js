0ddb3b47f8376773b5c5da03c501e4b581a488e6
rafmontenegro <rafaelcouto111@gmail.com>
2024-01-23 16:07:41 -0300

    feat: [ui]Input component

--- ui/src/pages/CreateContact/index.js
+++ ui/src/pages/CreateContact/index.js
@@ -1,5 +1,15 @@
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 export default function CreateContact() {
-  return <PageHeader title="New contact" />;
+  return (
+    <>
+      <PageHeader title="New contact" />
+      <Input type="text" placeholder="Name" />
+      <Select>
+        <option value="123">Discord</option>
+      </Select>
+    </>
+  );
 }
